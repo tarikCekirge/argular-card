102ec6ab04d5725d86e1f63ff4b41f32b52bdf85
Tarik Cekirge <tcekirge@gmail.com>
2022-12-23 19:57:42 +0300

    Angular Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card-project';
+  posts=[
+    {
+      title:"Nullam eu",
+      imageUrl:"assets/images/tree.jpeg",
+      content:"Praesent tincidunt cursus vestibulum. Nulla sit amet luctus metus. Proin tristique a mauris eu faucibus. Nam et velit in elit.      "
+    },
+    {
+      title:"Aenean tincidunt",
+      imageUrl:"assets/images/biking.jpeg",
+      content:"Praesent consequat et nibh nec ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec id ipsum et."
+    },
+    {
+      title:"Donec commodo",
+      imageUrl:"assets/images/mountain.jpeg",
+      content:"Nam vitae euismod odio, quis euismod neque. Pellentesque id nulla vel erat ultricies rutrum sit amet placerat mi. Nullam ut."
+    }
+  ]
 }
